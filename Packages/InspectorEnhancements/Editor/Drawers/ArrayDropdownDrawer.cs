1671f42ef834d8635a9ee05a389e33a66c98a515
Nenniana <Tsbchrist@gmail.com>
2024-10-30 18:11:48 +0100

    Add GetDropdownValues
    To return string values based on MemberType.

--- Packages/InspectorEnhancements/Editor/Drawers/ArrayDropdownDrawer.cs
+++ Packages/InspectorEnhancements/Editor/Drawers/ArrayDropdownDrawer.cs
@@ -8,7 +8,7 @@ namespace InspectorEnhancements
     public class ArrayDropdownDrawer : PropertyDrawer
     {
         private IMemberInfoProvider memberInfoProvider = new CacheMemberInfoProvider();
-
+        
         private void CreateDropdown(Rect position, SerializedProperty property, GUIContent label, string[] options)
         {
             if (options != null && options.Length > 0)
@@ -27,6 +27,46 @@ namespace InspectorEnhancements
             {
                 EditorGUI.LabelField(position, label.text, "Array is null or empty.");
             }
+        }
+
+        private string[] GetDropdownValues(string parameterName, SerializedProperty property) 
+        {
+            object target = property.serializedObject.targetObject;
+            
+            FieldInfo fieldInfo = memberInfoProvider.TryGetMemberInfo<FieldInfo>(target, parameterName);
+            if (fieldInfo != null)
+            {
+                return TryGetFieldStringValues();
+            }
+
+            PropertyInfo propertyInfo = memberInfoProvider.TryGetMemberInfo<PropertyInfo>(target, parameterName);
+            if (propertyInfo != null)
+            {
+                return TryGetPropertyStringValues();
+            }
+
+            MethodInfo methodInfo = memberInfoProvider.TryGetMemberInfo<MethodInfo>(target, parameterName);
+            if (methodInfo != null)
+            {
+                return TryGetMethodStringValues();
+            }
+
+            return null;
+        }
+
+        private static string[] TryGetMethodStringValues()
+        {
+            return null;
+        }
+
+        private string[] TryGetPropertyStringValues()
+        {
+            return null;
+        }
+
+        private string[] TryGetFieldStringValues()
+        {
+            return null;
         }
     }
 }
