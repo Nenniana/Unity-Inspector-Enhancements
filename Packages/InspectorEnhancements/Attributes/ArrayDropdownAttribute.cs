9e6ef4b7290514fcf052c45971d43152b9aa1417
Nenniana <Tsbchrist@gmail.com>
2024-10-30 22:08:17 +0100

    Implement IMethodOwner

--- Packages/InspectorEnhancements/Attributes/ArrayDropdownAttribute.cs
+++ Packages/InspectorEnhancements/Attributes/ArrayDropdownAttribute.cs
@@ -1,11 +1,18 @@
 namespace InspectorEnhancements
 {
-    public class ArrayDropdownAttribute : CustomPropertyAttribute
+    public class ArrayDropdownAttribute : CustomPropertyAttribute, IMethodOwner
     {
         public string Condition { get; private set; }
 
+        public object[] Parameters { get; private set; }
+
         public ArrayDropdownAttribute (string _condition) {
             Condition = _condition;
+        }
+
+        public ArrayDropdownAttribute (string _condition, object[] _parameters) {
+            Condition = _condition;
+            Parameters = _parameters;
         }
     }
 }
