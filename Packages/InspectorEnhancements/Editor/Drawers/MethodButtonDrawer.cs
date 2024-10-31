dd74f1d3b5eeae6d7a359c9de7e0dd20d7dd6793
Nenniana <Tsbchrist@gmail.com>
2024-10-31 11:48:14 +0100

    Add GetMethodParams

--- Packages/InspectorEnhancements/Editor/Drawers/MethodButtonDrawer.cs
+++ Packages/InspectorEnhancements/Editor/Drawers/MethodButtonDrawer.cs
@@ -1,15 +1,29 @@
+using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 
 namespace InspectorEnhancements
 {
     [CustomPropertyDrawer(typeof(MethodButtonAttribute))]
     public class MethodButtonDrawer : PropertyDrawer
     {
+        private readonly IMemberInfoProvider memberInfoProvider = new CacheMemberInfoProvider();
         // OnGUI Implementation
 
         // PropertyHeight Implementation
 
-        // GetMethodParams Implementation
+        private ParameterInfo[] GetMethodParams(object target, SerializedProperty property) 
+        {
+            MethodInfo methodInfo = memberInfoProvider.TryGetMemberInfo<MethodInfo>(target, property.name);
+
+            if (methodInfo == null)
+            {
+                Debug.LogError($"No MethodInfo found for {property.name} on {target.GetType()}.");
+                return null;
+            }
+
+            return methodInfo.GetParameters();
+        }
 
         // DrawMethodParams Implementation
         
