d2e1de711f367ffc43f8fbb7e123a81a6e474387
Nenniana <Tsbchrist@gmail.com>
2024-10-31 17:03:45 +0100

    Implement DrawPrimitiveField

--- Packages/InspectorEnhancements/Helpers/Interfaces/IFieldDrawer/BaseFieldDrawer.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IFieldDrawer/BaseFieldDrawer.cs
@@ -19,7 +19,7 @@ namespace InspectorEnhancements
             }
             if (type.IsPrimitive || type == typeof(string))
             {
-                // Draw primitive value
+                DrawPrimitiveField(label, value, isEditable);
             }
             else if (typeof(UnityEngine.Object).IsAssignableFrom(type))
             {
@@ -34,6 +34,34 @@ namespace InspectorEnhancements
         protected virtual void DrawUnityNativeType(string label, object value, Type type)
         {
             EditorGUILayout.ObjectField(label, (UnityEngine.Object)value, type, true);
+        }
+
+        protected virtual void DrawPrimitiveField(string label, object value, bool isEditable)
+        {
+            if (!isEditable)
+            {
+                EditorGUILayout.LabelField(label, value.ToString());
+                return;
+            }
+
+            switch (value)
+            {
+                case int intValue:
+                    EditorGUILayout.IntField(label, intValue);
+                    break;
+                case float floatValue:
+                    EditorGUILayout.FloatField(label, floatValue);
+                    break;
+                case bool boolValue:
+                    EditorGUILayout.Toggle(label, boolValue);
+                    break;
+                case string stringValue:
+                    EditorGUILayout.TextField(label, stringValue);
+                    break;
+                default:
+                    EditorGUILayout.LabelField(label, value.ToString());
+                    break;
+            }
         }
     }
 }
