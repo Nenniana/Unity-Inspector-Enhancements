550aded49a49029341ff9352b8fe7290c2e893ec
Nenniana <Tsbchrist@gmail.com>
2024-10-31 13:36:46 +0100

    Change cache functionality name

--- Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/CacheMemberInfoProvider.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/CacheMemberInfoProvider.cs
@@ -4,9 +4,14 @@ namespace InspectorEnhancements
 {
     public class CacheMemberInfoProvider : IMemberInfoProvider
     {
+        public TInfo[] TryGetAllMemberInfo<TInfo>(object target) where TInfo : MemberInfo
+        {
+            throw new System.NotImplementedException();
+        }
+
         public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName) where TInfo : MemberInfo
         {
-            TInfo memberInfo = CacheHelper<TInfo>.GetOrAdd(
+            TInfo memberInfo = CacheHelper<TInfo>.GetOrAddByName(
                 target, conditionName,
                 () => ReflectionHelper.FindMemberInfo<TInfo>(target, conditionName)
             );
