7cf731ebdb2016808a7b80201e4ae0f4ed9046f4
Nenniana <Tsbchrist@gmail.com>
2024-10-31 14:07:00 +0100

    Add TypeBasedCacheHelper
    Primarily for non-serialized properties.

---
+++ Packages/InspectorEnhancements/Helpers/General/TypeBasedCacheHelper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InspectorEnhancements
+{
+    public static class TypeBasedCacheHelper<TValue>
+    {
+        private static Dictionary<Type, List<TValue>> cache = new Dictionary<Type, List<TValue>>();
+
+        public static List<TValue> GetOrAddList(object[] target, Func<TValue[]> computeValue)
+        {
+            Type typeKey = target.GetType();
+            if (!cache.TryGetValue(typeKey, out var valueList))
+            {
+                valueList = computeValue().ToList();
+                cache[typeKey] = valueList;
+            }
+
+            return valueList;
+        }
+
+        public static void ClearCache()
+        {
+            cache.Clear();
+        }
+    }
+}
