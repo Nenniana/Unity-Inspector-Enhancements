d00be5fb7fa3e49dacfa05c0f67f7e9acd5d8967
Nenniana <Tsbchrist@gmail.com>
2024-10-31 11:22:07 +0100

    Add MethodButtonAttribute
    With both default and IMetholdHolder constructors.

---
+++ Packages/InspectorEnhancements/Attributes/MethodButtonAttribute.cs
@@ -0,0 +1,21 @@
+namespace InspectorEnhancements
+{
+    public class MethodButtonAttribute : CustomPropertyAttribute, IMethodOwner
+    {
+        public string Condition { get; private set; }
+        public object[] Parameters { get; private set; }
+
+        public MethodButtonAttribute() {}
+
+        public MethodButtonAttribute (string _condition)
+        {
+            Condition = _condition;
+        }
+
+        public MethodButtonAttribute (string _condition, params object[] _parameters)
+        {
+            Condition = _condition;
+            Parameters = _parameters;
+        }
+    }
+}
