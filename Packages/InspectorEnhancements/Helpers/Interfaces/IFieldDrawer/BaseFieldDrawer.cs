81cfdd07e58a88b1d9a26788275edb57644afc4b
Nenniana <Tsbchrist@gmail.com>
2024-10-31 17:00:27 +0100

    Add EvaluateType
    To split up functionality based on Type.

--- Packages/InspectorEnhancements/Helpers/Interfaces/IFieldDrawer/BaseFieldDrawer.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IFieldDrawer/BaseFieldDrawer.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor;
 
 namespace InspectorEnhancements
 {
@@ -7,6 +8,27 @@ namespace InspectorEnhancements
         public virtual void DrawField(string fieldName, object fieldValue, Type type, bool isEditable)
         {
             throw new NotImplementedException();
+        }
+
+        protected virtual void EvaluateType(string label, object value, Type type, bool isEditable)
+        {
+            if (value == null)
+            {
+                EditorGUILayout.LabelField(label, "null");
+                return;
+            }
+            if (type.IsPrimitive || type == typeof(string))
+            {
+                // Draw primitive value
+            }
+            else if (typeof(UnityEngine.Object).IsAssignableFrom(type))
+            {
+                // Draw Unity native value
+            }
+            else if (type.IsClass || type.IsValueType)
+            {
+                // Draw complex value
+            }
         }
     }
 }
