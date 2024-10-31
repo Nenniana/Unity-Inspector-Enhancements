659b2828ee651f2ea389f69cbf6af05502493c4a
Nenniana <Tsbchrist@gmail.com>
2024-10-31 11:38:54 +0100

    Add implementation skeleton

--- Packages/InspectorEnhancements/Editor/Drawers/MethodButtonDrawer.cs
+++ Packages/InspectorEnhancements/Editor/Drawers/MethodButtonDrawer.cs
@@ -5,6 +5,14 @@ namespace InspectorEnhancements
     [CustomPropertyDrawer(typeof(MethodButtonAttribute))]
     public class MethodButtonDrawer : PropertyDrawer
     {
+        // OnGUI Implementation
+
+        // PropertyHeight Implementation
+
+        // GetMethodParams Implementation
+
+        // DrawMethodParams Implementation
         
+        // InvokeMethod Implementation
     }
 }
