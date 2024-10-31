44f3b042619f7c18e16551570998d1d613e8febc
Nenniana <Tsbchrist@gmail.com>
2024-10-31 12:54:58 +0100

    Add TryGetAllMemberInfo

--- Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
@@ -5,5 +5,6 @@ namespace InspectorEnhancements
     public interface IMemberInfoProvider
     {
         public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName) where TInfo : MemberInfo;
+        public TInfo[] TryGetAllMemberInfo<TInfo>(object target, string conditionName) where TInfo : MemberInfo;
     }
 }
