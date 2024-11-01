c62291060435f40d486a9f2c57e44d0e18e00545
Nenniana <Tsbchrist@gmail.com>
2024-11-01 22:55:56 +0100

    Default values for interface BindingFlags

--- Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
@@ -6,7 +6,7 @@ namespace InspectorEnhancements
 {
     public interface IMemberInfoProvider
     {
-        public TInfo TryGetMemberInfo<TInfo>(Type type, string conditionName, BindingFlags bindingFlags) where TInfo : MemberInfo;
-        public List<TInfo> TryGetAllMemberInfo<TInfo>(Type type, BindingFlags bindingFlags) where TInfo : MemberInfo;
+        public TInfo TryGetMemberInfo<TInfo>(Type type, string conditionName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy) where TInfo : MemberInfo;
+        public List<TInfo> TryGetAllMemberInfo<TInfo>(Type type, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy) where TInfo : MemberInfo;
     }
 }
