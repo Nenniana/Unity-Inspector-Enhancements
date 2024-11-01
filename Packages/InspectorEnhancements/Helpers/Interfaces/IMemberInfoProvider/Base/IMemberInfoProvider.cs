14322f9a6d33c727d15c8db8e14b356a3ba3b991
Nenniana <Tsbchrist@gmail.com>
2024-11-01 22:46:18 +0100

    Implement choice of BindingFlags
    With defaults for compatibility.

--- Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/Base/IMemberInfoProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -5,7 +6,7 @@ namespace InspectorEnhancements
 {
     public interface IMemberInfoProvider
     {
-        public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName) where TInfo : MemberInfo;
-        public List<TInfo> TryGetAllMemberInfo<TInfo>(object target) where TInfo : MemberInfo;
+        public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName, BindingFlags bindingFlags) where TInfo : MemberInfo;
+        public List<TInfo> TryGetAllMemberInfo<TInfo>(object target, BindingFlags bindingFlags) where TInfo : MemberInfo;
     }
 }
--- Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/CacheMemberInfoProvider.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/IMemberInfoProvider/CacheMemberInfoProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -5,11 +6,11 @@ namespace InspectorEnhancements
 {
     public class CacheMemberInfoProvider : IMemberInfoProvider
     {
-        public List<TInfo> TryGetAllMemberInfo<TInfo>(object target) where TInfo : MemberInfo
+        public List<TInfo> TryGetAllMemberInfo<TInfo>(object target, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy) where TInfo : MemberInfo
         {
             List<TInfo> allMemberInfo = TypeBasedCacheHelper<TInfo>.GetOrAddList(
                 target, 
-                () => ReflectionHelper.FindAllMemberInfo<TInfo>(target)
+                () => ReflectionHelper.FindAllMemberInfo<TInfo>(target, bindingFlags)
             );
 
             if (allMemberInfo == null)
@@ -20,11 +21,11 @@ namespace InspectorEnhancements
             return allMemberInfo;
         }
 
-        public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName) where TInfo : MemberInfo
+        public TInfo TryGetMemberInfo<TInfo>(object target, string conditionName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy) where TInfo : MemberInfo
         {
             TInfo memberInfo = StringBasedCacheHelper<TInfo>.GetOrAdd(
                 target, conditionName,
-                () => ReflectionHelper.FindMemberInfo<TInfo>(target, conditionName)
+                () => ReflectionHelper.FindMemberInfo<TInfo>(target, conditionName, bindingFlags)
             );
 
             if (memberInfo == null)
