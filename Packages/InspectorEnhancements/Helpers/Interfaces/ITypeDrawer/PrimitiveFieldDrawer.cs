868357a09eeda446522623662f7f35b1a3cce64c
Nenniana <Tsbchrist@gmail.com>
2024-11-05 21:52:06 +0100

    Update to If/Else
    To avoid Switch case issues.

--- Packages/InspectorEnhancements/Helpers/Interfaces/ITypeDrawer/PrimitiveFieldDrawer.cs
+++ Packages/InspectorEnhancements/Helpers/Interfaces/ITypeDrawer/PrimitiveFieldDrawer.cs
@@ -8,29 +8,43 @@ namespace InspectorEnhancements
     {
         public bool Draw(string label, ref object value, Type type, bool isEditable)
         {
+            // Ensure the type is either a supported primitive or string
             if (!type.IsPrimitive && type != typeof(string))
                 return false;
 
             EditorGUI.BeginDisabledGroup(!isEditable);
 
-            switch (value)
+            try
             {
-                case int intValue:
-                    value = EditorGUILayout.IntField(label, intValue);
-                    break;
-                case float floatValue:
-                    value = EditorGUILayout.FloatField(label, floatValue);
-                    break;
-                case bool boolValue:
-                    value = EditorGUILayout.Toggle(label, boolValue);
-                    break;
-                case string stringValue:
-                    value = EditorGUILayout.TextField(label, stringValue);
-                    break;
-                default:
+                if (type == typeof(int))
+                {
+                    // Convert value to int if possible, otherwise use 0 as a default
+                    value = EditorGUILayout.IntField(label, (int)Convert.ChangeType(value, typeof(int)));
+                }
+                else if (type == typeof(float))
+                {
+                    // Convert value to float if possible, otherwise use 0f as a default
+                    value = EditorGUILayout.FloatField(label, (float)Convert.ChangeType(value, typeof(float)));
+                }
+                else if (type == typeof(bool))
+                {
+                    // Convert value to bool if possible, otherwise use false as a default
+                    value = EditorGUILayout.Toggle(label, (bool)Convert.ChangeType(value, typeof(bool)));
+                }
+                else if (type == typeof(string))
+                {
+                    // Convert value to string if possible, otherwise use empty string as a default
+                    value = EditorGUILayout.TextField(label, (string)Convert.ChangeType(value, typeof(string)));
+                }
+                else
+                {
                     Debug.LogWarning($"Unsupported primitive type: {type.Name}");
-                    EditorGUILayout.LabelField(label, value.ToString());
-                    break;
+                    EditorGUILayout.LabelField(label, value?.ToString() ?? "null");
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                Debug.LogError($"Invalid cast for field '{label}' with type '{type.Name}': {ex.Message}");
             }
 
             EditorGUI.EndDisabledGroup();
