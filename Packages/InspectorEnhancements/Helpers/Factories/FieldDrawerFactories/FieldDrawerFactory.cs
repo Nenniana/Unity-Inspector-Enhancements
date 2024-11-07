b7daab77658d56885c7498dbfc7f67a15cabedca
Nenniana <Tsbchrist@gmail.com>
2024-11-07 19:30:33 +0100

    Clean-up comments

--- Packages/InspectorEnhancements/Helpers/Factories/FieldDrawerFactories/FieldDrawerFactory.cs
+++ Packages/InspectorEnhancements/Helpers/Factories/FieldDrawerFactories/FieldDrawerFactory.cs
@@ -3,27 +3,24 @@ using System.Collections.Generic;
 namespace InspectorEnhancements
 {
     public static class FieldDrawerFactory
-{
-    // Create a default IFieldDrawer with dependencies passed in as parameters
-    public static IFieldDrawer CreateDefaultFieldDrawer(
-        IMemberInfoProvider memberInfoProvider,
-        EditorGUILayoutMethodProvider editorGUILayoutProvider)
     {
-        // Declare fieldDrawer variable so we can reference it in the delegate
-        BaseFieldDrawer fieldDrawer = null;
+        public static IFieldDrawer CreateDefaultFieldDrawer(
+            IMemberInfoProvider memberInfoProvider,
+            EditorGUILayoutMethodProvider editorGUILayoutProvider)
+        {
+            BaseFieldDrawer fieldDrawer = null;
 
-        fieldDrawer = new BaseFieldDrawer(
-            new List<ITypeDrawer>
-            {
-                new PrimitiveFieldDrawer(),
-                new UnityNativeFieldDrawer(),
-                new UnityStructFieldDrawer(editorGUILayoutProvider),
-                new ComplexFieldDrawer(() => fieldDrawer, memberInfoProvider)
-            }
-        );
+            fieldDrawer = new BaseFieldDrawer(
+                new List<ITypeDrawer>
+                {
+                    new PrimitiveFieldDrawer(),
+                    new UnityNativeFieldDrawer(),
+                    new UnityStructFieldDrawer(editorGUILayoutProvider),
+                    new ComplexFieldDrawer(() => fieldDrawer, memberInfoProvider)
+                }
+            );
 
-        // Inject the fieldDrawer reference into ComplexFieldDrawer after construction
-        return fieldDrawer;
+            return fieldDrawer;
+        }
     }
-}
 }
