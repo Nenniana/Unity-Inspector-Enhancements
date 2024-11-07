eb4408f3f7323c1c3c72ad403b1300db7fd76c69
Nenniana <Tsbchrist@gmail.com>
2024-11-07 18:44:09 +0100

    Implement injectable cache-based parameter access

---
+++ Packages/InspectorEnhancements/Editor/ParameterManagers/IParameterProvider/OverwriteableParameterProvider.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reflection;
+
+namespace InspectorEnhancements
+{
+    public class OverwriteableParameterProvider : IParameterProvider
+    {
+        public object GetOrAdd(string methodName, ParameterInfo parameter, Func<object> valueFactory)
+        {
+            string key = GetUniqueParameterName(methodName, parameter);
+            return OverwriteableStringCache<object>.GetOrAdd(typeof(object), key, valueFactory);
+        }
+
+        public object OverwriteOrAdd(string methodName, ParameterInfo parameter, object value)
+        {
+            string key = GetUniqueParameterName(methodName, parameter);
+            return OverwriteableStringCache<object>.OverwriteOrAdd(typeof(object), key, value);
+        }
+
+        private static string GetUniqueParameterName(string methodName, ParameterInfo parameter)
+        {
+            return methodName + "." + parameter.Name;
+        }
+    }
+}
