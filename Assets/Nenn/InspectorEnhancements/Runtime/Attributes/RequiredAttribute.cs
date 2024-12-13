b5c5f10481a287915e1cec352189e9298b238d5f
Nenniana <Tsbchrist@gmail.com>
2024-12-13 15:58:53 +0100

    Apply readonly

--- Assets/Nenn/InspectorEnhancements/Runtime/Attributes/RequiredAttribute.cs
+++ Assets/Nenn/InspectorEnhancements/Runtime/Attributes/RequiredAttribute.cs
@@ -6,7 +6,7 @@ namespace Nenn.InspectorEnhancements.Runtime.Attributes
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public class RequiredAttribute : CustomPropertyAttribute 
     {
-        public string ErrorMessage;
+        public readonly string ErrorMessage;
     
         public RequiredAttribute(string errorMessage = "Below field is required.") 
         {
